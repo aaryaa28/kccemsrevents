4c06a7f2490e0de17b070df0899a59e9a45ca847
aaryaa28 <141750844+aaryaa28@users.noreply.github.com>
2023-10-19 20:36:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const registerButton = document.getElementById("registerButton");
+const signupContainer = document.getElementById("signupContainer");
+
+registerButton.addEventListener("click", () => {
+    signupContainer.style.display = "block";
+});
